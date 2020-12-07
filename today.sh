abba5577dd3379893807ae46341f67d77c5198a0
Jérémie Roquet <jroquet@arkanosis.net>
2020-12-07 11:59:25 +0100

    Add bootstrap code

---
+++ today.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+SESSION="TODO FIXME"
+
+YEAR="$(date +%Y)"
+DAY="$(date +%-d)"
+
+curl -s --compressed "https://adventofcode.com/$YEAR/day/$DAY/input" -H "Cookie: session=$SESSION" > "$DAY.input"
+
+if ! [ -f "${DAY}_1.py" ] && ! [ -f "${DAY}_2.py" ]; then
+    cat <<EOF | tee "${DAY}_1.py" > "${DAY}_2.py"
+#! /usr/bin/env python3
+
+with open('$DAY.input', 'r') as file:
+  for line in file:
+    line = line.strip()
+    print(line)
+EOF
+    chmod a+x "${DAY}_1.py" "${DAY}_2.py"
+fi
+
+emacs -nw +6 "${DAY}_2.py" +6 "${DAY}_1.py" --eval "(global-set-key [f9] (lambda () (interactive) (run-python) (windmove-up) (python-shell-send-buffer)))" --eval "(global-set-key [f10] 'python-shell-send-buffer)"
